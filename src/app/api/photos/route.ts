0c032399f618a98e2b873bbc2920127abfce0be3
javier.castaner@col.vueling.com <xcastanyer>
2025-09-27 10:40:15 +0200

    Fix

--- src/app/api/photos/route.ts
+++ src/app/api/photos/route.ts
@@ -2,27 +2,27 @@ import { NextResponse } from "next/server";
 import cloudinary from "@/lib/cloudinary";
 
 export async function GET() {
-  try {
+    try {
 
-    const { resources } = await cloudinary.api.resources({
-      type: "upload",
-      resource_type: "image",
-      //prefix: folder + "/", // busca dentro de esa carpeta
-      max_results: 100,
-    });
+        const { resources } = await cloudinary.api.resources({
+            type: "upload",
+            resource_type: "image",
+            //prefix: folder + "/", // busca dentro de esa carpeta
+            max_results: 100,
+        });
 
-    // const urls = resources.map((file: any) => file.secure_url);
-        // Creamos el array de objetos con id y url
-    const urls = resources.map((res: CloudinaryResource, index: number) => ({
-      id: index + 1,
-      url: cloudinary.url(res.public_id, { width: 1600, crop: "scale" })
-    }));
 
- 
-    return NextResponse.json({ urls });
-  } catch (error: unknown) {
-    let message = "Unknown error";
-    if (error instanceof Error) message = error.message;
-    return NextResponse.json({ error: message }, { status: 500 });
-  }
+        const urls = resources.map((res: { public_id: string; secure_url: string }, index: number) => ({
+            id: index + 1,
+            url: cloudinary.url(res.public_id, { width: 1600, crop: "scale" }),
+        }));
+
+
+
+        return NextResponse.json({ urls });
+    } catch (error: unknown) {
+        let message = "Unknown error";
+        if (error instanceof Error) message = error.message;
+        return NextResponse.json({ error: message }, { status: 500 });
+    }
 }
