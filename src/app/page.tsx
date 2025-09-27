df81e28acf8291340844843e7da4a8a2e1af500b
javier.castaner@col.vueling.com <xcastanyer>
2025-09-27 10:45:48 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,39 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
+type Photo = {
+  id: number;
+  url: string;
+};
+
 export default function HomePage() {
-  // const [selected, setSelected] = useState<string | null>(null);
-    const [selectedPhoto, setSelectedPhoto] = useState({ id: 0, url: "" });
-    const [photos, setPhotos] = useState<string[]>([]);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [selectedPhoto, setSelectedPhoto] = useState<Photo>({ url: "", id: 0 });
 
   useEffect(() => {
     const fetchPhotos = async () => {
       const res = await fetch("/api/photos");
       const data = await res.json();
-      console.log(data);
-       // Si data.urls ya es [{id, url}] lo dejamos, si no, lo transformamos
-    const photosArray = data.urls?.map((item: string, index: number) => {
-      // Si item es string, lo convertimos a {id, url}
-      if (typeof item === "string") {
-        return { id: index + 1, url: item };
-      }
-      return item; // si ya es {id, url}, lo dejamos tal cual
-    }) || [];
-
-    setPhotos(photosArray);
+  
+      setPhotos(data.urls || []);
     };
     fetchPhotos();
   }, []);
 const prevPhoto = (e: React.MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
-  e.stopPropagation();
-  setSelectedPhoto({url:photos[(selectedPhoto.id - 2 + photos.length) % photos.length].url, id:(selectedPhoto.id - 2 + photos.length) % photos.length + 1});  
- 
+   e.stopPropagation();
+  const newIndex = (selectedPhoto.id - 2 + photos.length) % photos.length;
+  setSelectedPhoto({
+    url: photos[newIndex].url,
+    id: newIndex + 1,
+  });
 };
 const nextPhoto = (e: React.MouseEvent<HTMLDivElement | HTMLButtonElement>) => {
-  e.stopPropagation();
-  setSelectedPhoto({url:photos[(selectedPhoto.id) % photos.length].url, id:(selectedPhoto.id) % photos.length + 1}    );  
- 
+   e.stopPropagation();
+  const newIndex = (selectedPhoto.id % photos.length);
+  setSelectedPhoto({
+    url: photos[newIndex].url,
+    id: newIndex + 1,
+  });
 };
 const handleWheel = (e: React.WheelEvent<HTMLDivElement>) => {
   e.stopPropagation(); // evita que cierre el lightbox
